a064835ee45a8973560186f7f4a0aeda92970b11
SJKed <simonjurstedtskolan@gmail.com>
2022-11-11 14:27:37 +0100

    v-1.0.0 chages

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -1,4 +1,12 @@
-const timeHelper = ({startDate, endDate}) => {
-	return 'November'
-}
+import dayjs from 'dayjs'
+import 'dayjs/locale/en-gb.js'
+dayjs.locale('en-gb') // en-gb starts week on monday, en does not
+const yyyymmdd = 'YYYY-MM-DD' // useful for simple .format(yyyymmdd)
 
+const timeHelper = ({ startDate, endDate }) => {
+	startDate = dayjs(startDate)
+	endDate = dayjs(endDate)
+	if (startDate.diff(endDate, 'month') === 0) { return startDate.format('MMMM') }
+	if (startDate.diff(endDate, 'days') === -364) { return startDate.format('YYYY') }
+	if (startDate.diff(endDate, 'month') !== 0) { return 'No valid period' }
+}
